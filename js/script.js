42bd2871a0893275baa1139482afec4b50d5aed7
Giadagenovesi <giada.g@outlook.it>
2023-03-14 17:05:38 +0100

    Completed MILESTONE 2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 // inserisco tutte le immagini in un array.
 const imagesArray = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
 
-// Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+console.log(imagesArray);
 
-for (let i = 0; i < imagesArray.lenght; i++) {
+const sliderContainer = document.querySelector(".slider-items");
+console.log(sliderContainer);
+
+for (let i = 0; i < imagesArray.length; i++) {
     const currentImage = imagesArray[i];
 
     const sliderItem = `
         <div class="item">
             <img src="${currentImage}" alt="">
-        </div>` ;
+        </div>`;
 
     console.log(sliderItem);
+sliderContainer.innerHTML += sliderItem;
 }
+
+// Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
+
+const items = document.getElementsByClassName("item");
+items[0].classList.add("active");
+
