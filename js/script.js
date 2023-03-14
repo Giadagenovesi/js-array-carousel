2b6c8ce51282470588987e1c047a4ab7a3365bae
Giadagenovesi <giada.g@outlook.it>
2023-03-14 18:29:57 +0100

    Completed MILESTONE 3

--- js/script.js
+++ js/script.js
@@ -20,6 +20,41 @@ sliderContainer.innerHTML += sliderItem;
 
 // Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
 
+
 const items = document.getElementsByClassName("item");
-items[0].classList.add("active");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+let activeItemIndex = 0;
+items[activeItemIndex].classList.add("active");
+
+// Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+
+//bottone next
+nextBtn.addEventListener ("click", function() {
+    if (activeItemIndex < items.length - 1) {
+        //rimuovo classe active dall'elemento precedente
+        items[activeItemIndex].classList.remove("active");
+
+        // incremeto active index
+        activeItemIndex++;
+
+        //Aggiungo classe active all'elemento successivo
+        items[activeItemIndex].classList.add("active");
+    }
+});
+
+//bottone prev
+prevBtn.addEventListener ("click", function() {
+    if (activeItemIndex != 0 ) {
+        //rimuovo classe active dall'elemento precedente
+        items[activeItemIndex].classList.remove("active");
+
+        // diminuisco active index
+        activeItemIndex--;
+    
+        //Aggiungo classe active all'elemento successivo
+        items[activeItemIndex].classList.add("active");
+    } 
+});
 
